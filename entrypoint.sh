4ed10a316b29f8307413e118726917a99e88373c
Mikhail Chatillon <chmikhai@microsoft.com>
2020-03-29 12:53:54 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 echo 'start processing'
 echo $iothub_connection_string
-iotedgedev setup -i $iothub_connection_string   
+ls /github/home
+
+echo "***"
+ls /github/workspace
+
+echo "***"
+ls /github/workflow
+# iotedgedev setup -i $iothub_connection_string   
