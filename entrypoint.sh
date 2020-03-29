b8d434bb167c374fcf0cc07c69df10117d88d8bc
Mikhail Chatillon <chmikhai@microsoft.com>
2020-03-29 19:24:01 +0200

    rollback

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo 'start processing'
-echo $iothub_connection_string
 ls $GITHUB_WORKSPACE
-echo "***"
 iotedgedev build -f $GITHUB_WORKSPACE/lora/LoRaEngine/deployment.test.template.json
 
