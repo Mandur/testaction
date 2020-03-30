7e4e73c4518a9159d9363dd217d8e9d586366a31
Mikhail Chatillon <chmikhai@microsoft.com>
2020-03-30 10:42:41 +0200

    new

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'start processing'
-sudo echo $CONTAINER_REGISTRY_ADDRESS
-sudo echo $PKT_FWD_VERSION
-sudo iotedgedev build -f $GITHUB_WORKSPACE/lora/LoRaEngine/deployment.test.template.json
+echo $CONTAINER_REGISTRY_ADDRESS
+echo $PKT_FWD_VERSION
+iotedgedev build -f $GITHUB_WORKSPACE/lora/LoRaEngine/deployment.test.template.json
 
