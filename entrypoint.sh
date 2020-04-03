8c674020243d844c9743878097290706fe3f5fd5
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-03 17:15:45 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
-echo 'start processing'
-echo $CONTAINER_REGISTRY_ADDRESS
-echo $PKT_FWD_VERSION
-sudo iotedgedev build -f $GITHUB_WORKSPACE/lora/LoRaEngine/deployment.test.template.json
 
+# First argument is name of env var, second is default value.
+ensureEnvironmentVariableIsThere () {
+    if [ -z "${!1}" ]; then
+        echo "Environment Variable $1 not found, defaulting to $2"
+        export $1=$2
+    fi
+}
+
+echo 'start processing'
+echo "command : $Command"
+
+if [ -z "$Command" ]; then
+    Command="Build"
+fi
+
+ls
+
+if [ $Command = "Build" ]; then
+    ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
+    ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
+    sudo iotedgedev build -f $templateFilePath -P $defaultPlatform
+fi
+
+#push
+
+#deploy
