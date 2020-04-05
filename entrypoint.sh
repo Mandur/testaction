bb2cd636258850ed0b31fdcb12621a7cdf0448fb
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-05 15:38:32 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+printenv > .env
+cat .env
+sudo iotedgedev $1 -f $2
+
 # First argument is name of env var, second is default value.
-ensureEnvironmentVariableIsThere () {
-    if [ -z "${!1}" ]; then
-        echo "Environment Variable $1 not found, defaulting to $2"
-        export $1=$2
-    fi
-}
+# ensureEnvironmentVariableIsThere () {
+#     if [ -z "${!1}" ]; then
+#         echo "Environment Variable $1 not found, defaulting to $2"
+#         export $1=$2
+#     fi
+# }
 
-echo 'start processing'
-echo "command : $Command"
-echo "$GITHUB_WORKSPACE"
-if [ -z "$Command" ]; then
-    Command="Build"
-fi
+# echo 'start processing'
+# echo "command : $Command"
+# echo "$GITHUB_WORKSPACE"
+# if [ -z "$Command" ]; then
+#     Command="Build"
+# fi
 
-ls
+# ls
 
-if [ $Command = "Build" ]; then
-    ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
-    ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
-    sudo iotedgedev build -f $templateFilePath -P $defaultPlatform
-fi
+# if [ $Command = "Build" ]; then
+#     ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
+#     ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
+#     sudo iotedgedev build -f $templateFilePath -P $defaultPlatform
+# elif [ $Command = "Push" ]
+# elif [ $Command = "Deploy" ]
+# fi
 
 #push
 
