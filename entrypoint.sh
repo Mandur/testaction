973c9ec51d65d65de9b9df3863fcfb21d761d112
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-06 14:08:15 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,9 +21,8 @@ elif [ $1 = "deploy" ]; then
     sudo -E iotedgedev genconfig -f $2
     sudo -E az iot -h
     sudo az extension add --name azure-iot
-    cat "config/deployment.json"
-    sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --config-id "$IOT_EDGE_DEPLOYMENT_ID"
-    sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/deployment.json" --config-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId=$DEVICE_ID"
+    sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
+    sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/deployment.json" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId=$DEVICE_ID"
 fi
 
 #push
