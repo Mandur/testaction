0056b60db577a73aed45cfe3eccf11d24f852177
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-06 12:59:44 +0200

    Add

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-sudo -E iotedgedev $1 -f $2
-
 # First argument is name of env var, second is default value.
-# ensureEnvironmentVariableIsThere () {
-#     if [ -z "${!1}" ]; then
-#         echo "Environment Variable $1 not found, defaulting to $2"
-#         export $1=$2
-#     fi
-# }
+ensureEnvironmentVariableIsThere () {
+    if [ -z "${!1}" ]; then
+        echo "Environment Variable $1 not found, defaulting to $2"
+        export $1=$2
+    fi
+}
 
-# echo 'start processing'
-# echo "command : $Command"
-# echo "$GITHUB_WORKSPACE"
-# if [ -z "$Command" ]; then
-#     Command="Build"
-# fi
+ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
+ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
 
-# ls
-
-# if [ $Command = "Build" ]; then
-#     ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
-#     ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
-#     sudo iotedgedev build -f $templateFilePath -P $defaultPlatform
-# elif [ $Command = "Push" ]
-# elif [ $Command = "Deploy" ]
-# fi
+if [ $1 = "Build" ]; then
+    echo "Building iot edge module"
+    sudo -E iotedgedev $1 -f $2
+elif [ $1 = "Push" ]
+    echo "Pushing iot edge module"
+    sudo -E iotedgedev $1 -f $2
+elif [ $1 = "Deploy" ]
+    echo "Deploying iot edge module"
+    envsubst < $2
+    iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --config-id "$IOT_EDGE_DEPLOYMENT_ID"
+    az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content $2 --config-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId=$DEVICE_ID"
+fi
 
 #push
 
