45366fcc1bf3bbb541e28fc4a68d4868b667966b
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-06 13:08:06 +0200

    Correct entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,13 @@ ensureEnvironmentVariableIsThere () {
 ensureEnvironmentVariableIsThere "templateFilePath" "deployment.template.json"
 ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
 
-if [ $1 = "Build" ]; then
+if [ $1 = "build" ]; then
     echo "Building iot edge module"
     sudo -E iotedgedev $1 -f $2
-elif [ $1 = "Push" ]
+elif [ $1 = "push" ]; then
     echo "Pushing iot edge module"
     sudo -E iotedgedev $1 -f $2
-elif [ $1 = "Deploy" ]
+elif [ $1 = "deploy" ]; then
     echo "Deploying iot edge module"
     envsubst < $2
     iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --config-id "$IOT_EDGE_DEPLOYMENT_ID"
