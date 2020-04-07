a403117208fb804bd397c84cb64cf1b7792150aa
Mikhail Chatillon <chmikhai@microsoft.com>
2020-04-07 17:02:30 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,13 @@ ensureEnvironmentVariableIsThere "defaultPlatform" "amd64"
 
 if [ $1 = "build" ]; then
     echo "Building iot edge module"
-    sudo -E iotedgedev $1 -f $2
+    sudo -E iotedgedev $1
 elif [ $1 = "push" ]; then
     echo "Pushing iot edge module"
-    sudo -E iotedgedev $1 -f $2
+    sudo -E iotedgedev $1
 elif [ $1 = "deploy" ]; then
     echo "Deploying iot edge module"
-    sudo -E iotedgedev genconfig -f $2
+    sudo -E iotedgedev genconfig 
     sudo az extension add --name azure-iot
     sudo -E az iot edge deployment delete --login "$IOTHUB_CONNECTION_STRING" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID"
     sudo -E az iot edge deployment create --login "$IOTHUB_CONNECTION_STRING" --content "config/deployment.json" --deployment-id "$IOT_EDGE_DEPLOYMENT_ID" --target-condition "deviceId='$DEVICE_ID'"
